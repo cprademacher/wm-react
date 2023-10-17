518049b4d62dc0746fc724e1694ba6a778bbd354
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-10-17 16:06:34 -0500

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { Button } from "./Button";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -30,7 +29,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
-            WM <FontAwesomeIcon icon={'faMountainSun'} />
+            WM <i class="fa-solid fa-person-snowboarding"></i>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
