b7b281c12d93f9f9849db5b9cef84463cb403561
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-12-24 21:13:15 -0600

    Got rid of an error, but when calling the api it starts as null and returns as undefined

--- src/components/FutureWeatherBox.js
+++ src/components/FutureWeatherBox.js
@@ -7,8 +7,8 @@ export default function FutureWeatherForecast() {
         <h3>Five Day Forecast:</h3>
       </div>
 
-      <div class="forecast-days row">
-        <div class="forecast-day col-xs-6 col-lg-2" id="day1">
+      <div className="forecast-days row">
+        <div className="forecast-day col-xs-6 col-lg-2" id="day1">
           <h4 id="d1date">Today's Date</h4>
           <img src="" id="d1weathericon" alt="day 1 icon" />
           <p id="d1temp">Temp: </p>
@@ -16,7 +16,7 @@ export default function FutureWeatherForecast() {
           <p id="d1humidity">Humidity: </p>
         </div>
 
-        <div class="forecast-day col-xs-6 col-lg-2" id="day2">
+        <div className="forecast-day col-xs-6 col-lg-2" id="day2">
           <h4 id="d2date">Today's Date</h4>
           <img src="" id="d2weathericon" alt="day 2 icon" />
           <p id="d2temp">Temp: </p>
@@ -24,7 +24,7 @@ export default function FutureWeatherForecast() {
           <p id="d2humidity">Humidity: </p>
         </div>
 
-        <div class="forecast-day col-xs-6 col-lg-2" id="day3">
+        <div className="forecast-day col-xs-6 col-lg-2" id="day3">
           <h4 id="d3date">Today's Date</h4>
           <img src="" id="d3weathericon" alt="day 3 icon" />
           <p id="d3temp">Temp: </p>
@@ -32,7 +32,7 @@ export default function FutureWeatherForecast() {
           <p id="d3humidity">Humidity: </p>
         </div>
 
-        <div class="forecast-day col-xs-6 col-lg-2" id="day4">
+        <div className="forecast-day col-xs-6 col-lg-2" id="day4">
           <h4 id="d4date">Today's Date</h4>
           <img src="" id="d4weathericon" alt="day 4 icon" />
           <p id="d4temp">Temp: </p>
@@ -40,7 +40,7 @@ export default function FutureWeatherForecast() {
           <p id="d4humidity">Humidity: </p>
         </div>
 
-        <div class="forecast-day col-xs-6 col-lg-2" id="day5">
+        <div className="forecast-day col-xs-6 col-lg-2" id="day5">
           <h4 id="d5date">Today's Date</h4>
           <img src="" id="d5weathericon" alt="day 5 icon" />
           <p id="d5temp">Temp: </p>
