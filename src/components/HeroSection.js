a38dac6b27538bc8bab30a84366dfa02690ce12c
Cody Rademacher <codyrademacher@Codys-MBP.lan>
2024-01-17 13:59:43 -0600

    Testing a new video src via s3

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ import "./HeroSection.css";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-3.mp4" autoPlay loop muted></video>
+      <video src="s3://which-mountain/video-3.mp4" autoPlay loop muted></video>
       <h1>Which Mountain</h1>
       <p>Scroll to see all mountains or choose your pass!</p>
       <div className="hero-btns">
